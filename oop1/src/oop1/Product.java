5529fa18dd35245a2aa7bf5119281f17240f7812
Yiğit Levent Akkuş <yigit10kasimm@gmail.com>
2022-09-22 00:40:15 +0300

    Ders Sonu

---
+++ oop1/src/oop1/Product.java
@@ -0,0 +1,52 @@
+package oop1;
+
+//PascalCase
+public class Product {
+	private String name;
+	// camelCase
+	private double unitprice;
+	private double discount;
+	private String imageUrl;
+	private int unitsInStock;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getUnitprice() {
+		return unitprice;
+	}
+
+	public void setUnitprice(double unitprice) {
+		this.unitprice = unitprice;
+	}
+
+	public double getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public int getUnitsInStock() {
+		return unitsInStock;
+	}
+
+	public void setUnitsInStock(int unitsInStock) {
+		this.unitsInStock = unitsInStock;
+	}
+
+}
--- oop1/src/oop1/main.java
+++ oop1/src/oop1/main.java
@@ -3,8 +3,54 @@ package oop1;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String mesaj = "Vade oranı";
 
+		Product product1 = new Product();
+		product1.setName("Delongi Kahve Makinesi");
+		product1.setDiscount(7);
+		product1.setUnitprice(7500);
+		product1.setUnitsInStock(3);
+		product1.setImageUrl("image1.jpg");
+
+		Product product2 = new Product();
+		product2.setName("Smeg Kahve Makinesi");
+		product2.setDiscount(7);
+		product2.setUnitprice(7500);
+		product2.setUnitsInStock(3);
+		product2.setImageUrl("image2.jpg");
+
+		Product product3 = new Product();
+		product3.setName("Kitchen Aid Kahve Makinesi");
+		product3.setDiscount(7);
+		product3.setUnitprice(7500);
+		product3.setUnitsInStock(3);
+		product3.setImageUrl("image3.jpg");
+
+		Product[] products = { product1, product2, product3 };
+
+		System.out.println("<ul>");
+		for (Product product : products) {
+			System.out.println("<li>" + product.getName() + "</li>");
+		}
+		System.out.println("</ul>");
+		
+		IndividualCustomer individualCustomer = new IndividualCustomer();
+		individualCustomer.setId(1);
+		individualCustomer.setPhone("052222222");
+		individualCustomer.setCustomerNumber("1235");
+		individualCustomer.setFirstName("Yigit");
+		individualCustomer.setLastName("Akkus");
+		
+		CorporateCustomer corporateCustomer = new CorporateCustomer();
+		corporateCustomer.setId(2);
+		corporateCustomer.setCompanyName("Kodlama.io");
+		corporateCustomer.setPhone("05313543131");
+		corporateCustomer.setTaxNumber("32184651684");
+		corporateCustomer.setCustomerNumber("54321");
+		
+		Customer[] customers = {individualCustomer,corporateCustomer};
+		
+		
 	}
 
 }
